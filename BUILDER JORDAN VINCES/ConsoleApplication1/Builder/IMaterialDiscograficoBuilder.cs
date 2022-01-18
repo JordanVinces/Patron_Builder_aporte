7ffa2cdb2ddacb759a68210de5c717d51fdaddf6
JordanVinces <94881562+JordanVinces@users.noreply.github.com>
2022-01-17 21:05:15 -0500

    Add files via upload

---
+++ BUILDER JORDAN VINCES/ConsoleApplication1/Builder/IMaterialDiscograficoBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1.Entidades;
+
+namespace ConsoleApplication1.Builder
+{
+
+    //CREACION DE LA INTERFAZ
+    // ESTOS METODOS DEVUELVEN UN MATERIAS DISCOGRAFICO BUILDER
+    public interface IMaterialDiscograficoBuilder
+    {
+        MaterialDiscograficoBuilder ConNombre(String nombre);
+        MaterialDiscograficoBuilder TienePrecio(Double precio);
+        MaterialDiscograficoBuilder EnStock(int stock);
+
+        MaterialDiscograficoBuilder ComoGenero(Genero genero);
+        MaterialDiscograficoBuilder ComoAutor(Autor autor);
+        MaterialDiscograficoBuilder ComoTipoMaterialDiscografico(TipoMaterialDiscografico tipoMaterialDiscografico);
+        MaterialDiscograficoBuilder ComoCanciones(List<Cancion> canciones);
+    }
+}
